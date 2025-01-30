5786e910abe2a352bf2b6e65f975a74178997912
Daniel Kuroski <daniel.kuroski@gmail.com>
2025-01-30 07:01:51 +0100

    wip

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	f, err := os.Open("image.jpeg")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer f.Close()
+
+	image, imageType, err := image.Decode(f)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println(image, imageType)
 }
